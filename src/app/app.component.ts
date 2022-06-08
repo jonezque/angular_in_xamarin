20bd2200fea921f243dbd8634ed3773d4f0ddfb9
Artem Brilev <razdvajonez@mail.ru>
2022-06-08 12:18:41 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { DataService } from './data.service';
+
+declare function invokeCSharpAction(data: string): void;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'ang-elf_app';
+export class AppComponent implements OnInit {
+  form = new FormGroup({
+    company: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
+
+  constructor(
+    private dataSerivce: DataService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.dataSerivce.data$.subscribe((data) => {
+      this.form.setValue(data);
+      this.cdr.detectChanges();
+    });
+  }
+
+  onClick() {
+    console.log('submitted', this.form.getRawValue());
+    if (invokeCSharpAction)
+      invokeCSharpAction(JSON.stringify(this.form.getRawValue()));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataService } from './data.service';
+
+const service = new DataService();
+(window as any).updatetextonwebview = (data: string) =>
+  service.update(JSON.parse(data));
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: DataService, useValue: service }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
